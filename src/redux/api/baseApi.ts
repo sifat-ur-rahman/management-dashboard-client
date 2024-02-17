3d32393f74a1a738776f36734e3cd03d3b046697
sifat <md.sifat.ur.rahman2702@gmail.com>
2024-02-17 19:04:53 +0600

    add

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const baseApi = createApi({
   reducerPath: "baseApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:5000/api",
+    baseUrl: "https://server-side-ass-6.vercel.app/api",
     credentials: "include",
   }),
   tagTypes: ["sale", "product", "addToCard"],
